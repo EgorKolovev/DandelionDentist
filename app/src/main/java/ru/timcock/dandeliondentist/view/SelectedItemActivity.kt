373e8787407375c06f0669df44f48787a8add4ef
KovalevEgor14 <olegnorilsk77@gmail.com>
2023-02-11 13:39:08 +0500

    backup

---
+++ app/src/main/java/ru/timcock/dandeliondentist/view/SelectedItemActivity.kt
@@ -0,0 +1,12 @@
+package ru.timcock.dandeliondentist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.timcock.dandeliondentist.R
+
+class SelectedItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selected_item)
+    }
+}
