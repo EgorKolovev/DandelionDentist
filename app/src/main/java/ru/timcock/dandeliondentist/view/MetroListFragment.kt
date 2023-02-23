1649d86630141effc8697bf5666dcfd9bae7f561
KovalevEgor14 <olegnorilsk77@gmail.com>
2023-02-24 00:03:43 +0500

    add checkbox listitem

---
+++ app/src/main/java/ru/timcock/dandeliondentist/view/MetroListFragment.kt
@@ -0,0 +1,55 @@
+package ru.timcock.dandeliondentist.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.timcock.dandeliondentist.R
+import ru.timcock.dandeliondentist.databinding.FragmentMetroListBinding
+
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class MetroListFragment : Fragment() {
+
+    private var param1: String? = null
+    private var param2: String? = null
+
+    lateinit var binding: FragmentMetroListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_metro_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding= FragmentMetroListBinding.bind(view)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MetroListFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
---
+++ app/src/main/java/ru/timcock/dandeliondentist/view/WhereSortActivity.kt
@@ -0,0 +1,12 @@
+package ru.timcock.dandeliondentist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.timcock.dandeliondentist.R
+
+class WhereSortActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_where_sort)
+    }
+}
